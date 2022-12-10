1885c20a348fcbf7c49494b8faa3bb376ee33149
iamsplash <111317032+Splashcard04@users.noreply.github.com>
2022-12-09 23:19:16 -0800

    hopefully fixed this

--- src/misc/logMapData.ts
+++ src/misc/logMapData.ts
@@ -1,11 +1,11 @@
-import { activeDiff, getBaseEnvironment } from "https://deno.land/x/remapper@3.0.0/src/mod.ts"
+import { activeDiff } from "https://deno.land/x/remapper@3.0.0/src/mod.ts"
 
 export function logMapData(objects?: boolean, environment?: boolean, fakeArray?: boolean) {
     const map = activeDiff
     
     if(objects !== undefined && objects === true) {
         console.log('\n======================Objects==========================\n')
-        console.log(`\n Notes:`map.notes.length, `\n Walls:`, map.walls.length, `\n bombs:`, map.bombs.length)
+        console.log(`\n Notes:`, map.notes.length, `\n Walls:`, map.walls.length, `\n bombs:`, map.bombs.length)
     }
     if(environment !== undefined && environment === true) {
         console.log(`==========================Environment======================`)
@@ -13,6 +13,6 @@ export function logMapData(objects?: boolean, environment?: boolean, fakeArray?:
     }
     if(fakeArray !== undefined && fakeArray === true) {
         console.log(`========================fake objects=======================`)
-        console.log(`\n Fake notes:` map.fakeNotes.length, `\n Fake walls:`, map.fakeWalls.length, `\n Fake bombs:`, map.fakeBombs.length, `\n Fake Chains: `, map.fakeChains.length)
+        console.log(`\n Fake notes:`, map.fakeNotes.length, `\n Fake walls:`, map.fakeWalls.length, `\n Fake bombs:`, map.fakeBombs.length, `\n Fake Chains: `, map.fakeChains.length)
     }
 }
