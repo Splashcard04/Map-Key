6bf8545054bcd24b6c8b699f6223605d851a9207
iamsplash <111317032+Splashcard04@users.noreply.github.com>
2022-12-10 07:38:46 -0800

    guys theres even more small fixes

--- src/utils/noteFilter.ts
+++ src/utils/noteFilter.ts
@@ -1,4 +1,4 @@
-import { notesBetween, Vec2, Note } from "https://deno.land/x/remapper@3.0.0/src/mod.ts"
+import { notesBetween, Vec2, Note, Json } from "https://deno.land/x/remapper@3.0.0/src/mod.ts"
 
 /**
 * filters notes at certain positions
@@ -10,13 +10,21 @@ import { notesBetween, Vec2, Note } from "https://deno.land/x/remapper@3.0.0/src
 
 
 export class noteFilter {
-  positions(positions: Vec2[]) {
-    this.positions = positions
+
+    json: Json = {}
+
+    import(json: Json) {
+        this.json = json
+        return this
+    }
+
+  positions(positions: Vec2) {
+    this.json.positions = positions
   }
   constructor(timeStart: number, timeEnd: number, forNote: (n: Note) => void) {
     notesBetween(timeStart, timeEnd, n => {
       let pass = false;
-      this.positions.forEach(p => {
+      this.json.positions.forEach((p: number[]) => {
         if (n.x === p[0] && n.y === p[1]) pass = true;
       });
       if (pass) forNote(n);
