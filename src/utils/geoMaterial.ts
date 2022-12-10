1feb300f9fb4030f1aa40609c4173579760f2589
iamsplash <111317032+Splashcard04@users.noreply.github.com>
2022-12-10 07:34:04 -0800

    fix some stuff

--- src/utils/geoMaterial.ts
+++ src/utils/geoMaterial.ts
@@ -1,4 +1,4 @@
-import { activeDiff, GeometryMaterial, Geometry, GEO_TYPE, Vec3 } from "https://deno.land/x/remapper@3.0.0/src/mod.ts" 
+import { activeDiff, GeometryMaterial, Geometry, GEO_TYPE, Vec3, Json } from "https://deno.land/x/remapper@3.0.0/src/mod.ts" 
 
 type addGroupSettings = {
     // deno-lint-ignore no-explicit-any
@@ -9,6 +9,15 @@ type addGroupSettings = {
 }
 
 export class geoMaterail {
+
+    json: Json = {}
+
+    import(json: Json) {
+        this.json = json
+        return this
+    }
+  
+
     constructor(name: string, material: GeometryMaterial) {
         const map = activeDiff
         
@@ -17,7 +26,7 @@ export class geoMaterail {
 
     }
     addGroup(addGroup: addGroupSettings) {
-        if(addGroupSettings.scale == undefined) { this.json.scale = [1, 1, 1]} else { this.json.scale = addGroupSettings.scale}
+        if(addGroup.scale == undefined) { this.json.scale = [1, 1, 1]} else { this.json.scale = addGroup.scale}
         addGroup.sceneName.addPrimaryGroups(
             addGroup.blenderMatName,
             new Geometry(addGroup.geoType, geoMaterail.name+"Material"),
