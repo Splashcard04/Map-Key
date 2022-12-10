a1edb89690d22e45712f76111738bdd8d647ec93
iamsplash <111317032+Splashcard04@users.noreply.github.com>
2022-12-10 07:54:03 -0800

    huh

---
+++ src/exports.ts
@@ -0,0 +1,11 @@
+export * from './src/utils/geoMaterial.ts'
+export * from './src/utils/animatePlayer.ts'
+export * from './src/utils/despawner.ts'
+export * from './src/utils/general.ts'
+export * from './src/utils/noteFilter.ts'
+export * from './src/utils/noteFilter.ts'
+export * from './src/utils/notePath.ts'
+export * from './src/utils/sceneGroup.ts'
+
+
+export * from './src/misc/logMapData.ts'
