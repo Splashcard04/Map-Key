f74dace18e7caad7ffe518a85e0d310aecc9951d
iamsplash <111317032+Splashcard04@users.noreply.github.com>
2022-12-10 08:00:37 -0800

    uh

--- src/exports.ts
+++ src/exports.ts
@@ -3,7 +3,6 @@ export * from './src/utils/animatePlayer.ts'
 export * from './src/utils/despawner.ts'
 export * from './src/utils/general.ts'
 export * from './src/utils/noteFilter.ts'
-export * from './src/utils/noteFilter.ts'
 export * from './src/utils/notePath.ts'
 export * from './src/utils/sceneGroup.ts'
 
