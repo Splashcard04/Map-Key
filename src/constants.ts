bf77541f9f5f44b6de1a1e8509cd287011c032ad
iamsplash <111317032+Splashcard04@users.noreply.github.com>
2022-12-14 17:32:38 +0000

    iregular expressions

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+import { ColorType } from "https://deno.land/x/remapper@3.0.0/src/mod.ts"
+
 //Property presets for BlenderFrameMath
 export type BFM_PROPS = 
     "_beatTime" |
@@ -7,8 +9,28 @@ export type BFM_PROPS =
     
 export const Env = {
     gaga: {
-        Aurora: ,
-        Lightning: ,
-        SolidLaser: ,
+        Aurora: "Aurora\\.\\[1\\]AuroraSecondary$",
+        Lightning: "[3\\]LightingWithTarget$",
+        solidLaser: "[0\\]FrontLaserL$",
+        directionalLight: "DirectionalLights\\.\\[0\\]DirectionalLightFront$"
+    },
+    billie: {
+        directionalLight: "DayAndNight\\.\\[0\\]Day\\.\\[1\\]DirectionalLightFront$",
+        solidLaser: "[46\\]BottomPairLasers\\.\\[0\\]PillarL\\.\\[0\\]RotationBaseL\\.\\[0\\]LaserLH"
+    },
+
+    all: {
+        cinemaScreen: "CinemaScreen$",
+        cinemaDirLight: "CinemaDirectionalLight$",
+        mirror: "PlayersPlace\\.\\[d*\\]Mirror"
+    }
+}
+
+export const colors = {
+    rgb: {
+        pink : [] as ColorType,
+        orange: [] as ColorType,
+        yellow: [] as ColorType,
+        brown: [] as ColorType
     }
 }
