49d606f3f696c431e20b4fda9f11b728a1d58479
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2022-12-19 15:41:09 +1030

    Update shapeGenerator.ts
    Added `iterateTrack` param and docs.

--- src/utils/shapeGenerator.ts
+++ src/utils/shapeGenerator.ts
@@ -11,6 +11,7 @@ export class shapeGenerator {
      * @param material The name of the material to use for the shape (create your own beforehand)
      * @param track Track to apply to the shape.
      * @param innercorners Changes the way that corners are joined. Triangles look better (imo) with inner corners.
+     * @param iterateTrack (Default = true) Changes the track value for each piece of the shape. False: every piece will have the same track. True: each piece will have the track `${track}_${i}` where {0 <= i < sides}
      * @author Aurellis
      */
     constructor(
@@ -21,7 +22,8 @@ export class shapeGenerator {
         public scale: Vec3 = [1,1,1],
         public rotation: Vec3 = [0,0,0],
         public innercorners?: boolean,
-        public track?: string
+        public track?: string,
+        public iterateTrack: boolean = true
         ){
         this.material = material;
         this.sides = sides;
@@ -31,12 +33,16 @@ export class shapeGenerator {
         this.rotation = rotation;
         this.innercorners = innercorners;
         this.track = track;
+        this.iterateTrack = iterateTrack;
         }
         push(){
             const cube = new Geometry("Cube", this.material);
             for(let side = 0; side < this.sides; side++){
-                if(this.track){
+                if(this.track && this.iterateTrack){
                     cube.track.value = `${this.track}_${side}`;
+                }
+                else if(this.track && !this.iterateTrack){
+                    cube.track.value = this.track;
                 }
                 const angle = Math.PI*2*side/this.sides
                 const pos = rotatePoint([-Math.sin(angle)*this.radius,-Math.cos(angle)*this.radius,0],this.rotation);
