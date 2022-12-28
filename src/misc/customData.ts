96c6dee7321ac201d0a0d1c6b1ce6ec8733ff4ac
iamsplash <111317032+Splashcard04@users.noreply.github.com>
2022-12-28 13:41:02 -0800

    added (not yet working) custom data class

---
+++ src/misc/customData.ts
@@ -0,0 +1,7 @@
+import { Wall, Note } from "https://deno.land/x/remapper@3.0.0/src/mod.ts"
+
+export class customData{
+   constructor(forData: (d: Wall | Note | Environment){
+      forData(d)
+   }
+}
