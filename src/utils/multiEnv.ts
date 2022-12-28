d6a7b0209d592da3a033a5097a4986cf915f0f33
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2022-12-28 20:09:17 +1030

    Update multiEnv.ts
    Back to a class now.

--- src/utils/multiEnv.ts
+++ src/utils/multiEnv.ts
@@ -1,43 +1,42 @@
 import { Environment, LOOKUP } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
 
-// Ok, we can use this later
-// export class multiEnv {
-//     /**
-//      * A class to aid in the manipulation of multiple environment pieces at once.
-//      * @param ids The ids for your environment pieces. Including the lookup that should be used for each.
-//      * @param forEach The code to execute on each env piece. Don't push your envs in here, They will be pushed when you use the .push() method on this class.
-//      * @example new multiEnv([["Sun","Contains"],["Clouds$","Regex"]],(env) => { env.active = false; }).push();
-//      * @author Aurellis
-//      */
-//     constructor(public ids: [string, LOOKUP][], public forEach?: (x: Environment) => void){
-//         this.ids = ids;
-//         this.forEach = forEach;
-//     }
-//     /**
-//      * Pushes the envs to the diff.
-//      */
-//     push(){
-//         this.ids.forEach(x =>{
-//             const env = new Environment(x[0],x[1]);
-//             if(this.forEach){
-//                 this.forEach(env);
-//             }
-//             env.push();
-//         })
-//     }
-// }
-
-// It's a function for now, so the push() statements need to be made in the forEach section.
-/**
- * Function to aid the manipulation of multiple environment pieces at once.
- * @param ids The ids and lookup for each environment piece.
- * @param forEach The code to execute for each environment piece.
- * @example multiEnv([["Sun","Contains"],["Clouds$","Regex"]], (env) => { env.active = false; env.push()})
- * @author Aurellis
- */
-export function multiEnv(ids: [string, LOOKUP][], forEach: (x: Environment) => void){
-    ids.forEach(x =>{
-        const env = new Environment(x[0],x[1]);
-        forEach(env);
-    });
+export class multiEnv {
+    /**
+     * A class to aid in the manipulation of multiple environment pieces at once.
+     * @param ids The ids for your environment pieces. Including the lookup that should be used for each.
+     * @param forEach The code to execute on each env piece. Don't push your envs in here, They will be pushed when you use the .push() method on this class.
+     * @example new multiEnv([["Sun","Contains"],["Clouds$","Regex"]],(env) => { env.active = false; }).push();
+     * @author Aurellis
+     */
+    constructor(public ids: [string, LOOKUP][], public forEach?: (x: Environment) => void){
+        this.ids = ids;
+        this.forEach = forEach;
+    }
+    /**
+     * Pushes the envs to the diff.
+     * If you had a `push()` statement in the `forEach` section, your environments will be pushed twice.
+     */
+    push(){
+        this.ids.forEach(x =>{
+            const env = new Environment(x[0],x[1]);
+            if(this.forEach){
+                this.forEach(env);
+            }
+            env.push();
+        })
+    }
 }
+
+// /**
+//  * Function to aid the manipulation of multiple environment pieces at once.
+//  * @param ids The ids and lookup for each environment piece.
+//  * @param forEach The code to execute for each environment piece.
+//  * @example multiEnv([["Sun","Contains"],["Clouds$","Regex"]], (env) => { env.active = false; env.push()})
+//  * @author Aurellis
+//  */
+// export function multiEnv(ids: [string, LOOKUP][], forEach: (x: Environment) => void){
+//     ids.forEach(x =>{
+//         const env = new Environment(x[0],x[1]);
+//         forEach(env);
+//     });
+// }
