6e8eaab174a888d103a19e53803c257c170fbb20
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2022-12-30 15:11:37 +1030

    Comments!
    So that I remember how this works.

--- src/utils/shapeGenerator.ts
+++ src/utils/shapeGenerator.ts
@@ -21,38 +21,54 @@ export class shapeGenerator {
         public position: Vec3 = [0,0,0],
         public scale: Vec3 = [1,1,1],
         public rotation: Vec3 = [0,0,0],
-        public innercorners?: boolean,
-        public track?: string,
+        public innercorners: boolean = false,
+        public track: string | undefined = undefined,
         public iterateTrack: boolean = true
-        ){
-        this.material = material;
-        this.sides = sides;
-        this.radius = radius;
-        this.position = position;
-        this.scale = scale;
-        this.rotation = rotation;
-        this.innercorners = innercorners;
-        this.track = track;
-        this.iterateTrack = iterateTrack;
-        }
+        ){}
         push(){
             const cube = new Geometry("Cube", this.material);
             for(let side = 0; side < this.sides; side++){
+                // Track assignment
                 if(this.track && this.iterateTrack){
                     cube.track.value = `${this.track}_${side}`;
                 }
                 else if(this.track && !this.iterateTrack){
                     cube.track.value = this.track;
                 }
-                const angle = Math.PI*2*side/this.sides
+
+                // Determine that angle of the side (could have used rotatePoint here, but this was a bit easier)
+                const angle = Math.PI*2*side/this.sides;
+
+                // Define the rotated position of the shape
                 const pos = rotatePoint([-Math.sin(angle)*this.radius,-Math.cos(angle)*this.radius,0],this.rotation);
+
+                // Apply the offset position to the rotated position
                 cube.position = [pos[0]+this.position[0],pos[1]+this.position[1],pos[2]+this.position[2]];//dumb arrAdd
+
+                /*
+                So that I remember how this math works - Aurellis
+
+                The scale is determined by using the "Length of a chord in a circle formula".
+                l = 2rsin(theta/2)
+                => rsin(theta/2) = l/2 (since l/2 is the opp of a triangle formed by r, l/2, and the line between [0,0,0] and the midpoint of l)
+                => since r = hyp and l/2 = opp, therefore sin(theta/2) = 0.5l/r.
+                This works for a chord where the ends are r distance from [0,0,0].
+
+                but our case uses a chord where the midpoint is r distance from [0,0,0],
+                therefore tan is applicable as opp/adj rather than opp/hyp since r is now adj rather than hyp.
+                giving the formula 2rTan(theta/2). where theta = 2pi/2sides or simplified pi/sides.
+
+                then, to account for the fact that the line is 2d rather than 1d,
+                the r value can either be radius-scale for the innermost corner to touch,
+                or radius+scale for the outermost corners to touch.
+                */
                 if(this.innercorners){
                     cube.scale = [(this.radius-this.scale[1]/2)*Math.tan(Math.PI/this.sides)*2, this.scale[1], this.scale[2]];
                 }
                 else{
                     cube.scale = [(this.radius+this.scale[1]/2)*Math.tan(Math.PI/this.sides)*2, this.scale[1], this.scale[2]];
                 }
+
                 cube.rotation = [this.rotation[0],this.rotation[1],this.rotation[2]-180*angle/Math.PI];
                 cube.push();
                 
