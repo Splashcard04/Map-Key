49648dd16bbf52c68bf4cef44ef4b6fa94769939
iamsplash <111317032+Splashcard04@users.noreply.github.com>
2022-12-29 23:08:36 -0800

    combine params

--- src/utils/sceneGroup.ts
+++ src/utils/sceneGroup.ts
@@ -1,4 +1,4 @@
-import { Environment, Geometry, GroupObjectTypes, Json, ModelScene, RMLog } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
+import { Environment, Geometry, GroupObjectTypes, Json, ModelScene, RMLog, Vec3 } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
 import { logFunctionss } from "./general.ts" 
 /**
  * @param object geometry or environment object for your group
@@ -7,95 +7,43 @@ import { logFunctionss } from "./general.ts"
  * @author splashcard__
  */
 
-export class lightGroup {
-    json: Json = {}
-
-    import(json: Json) {
-        this.json = json
-        return this
-    }
-
-    amount(ammount: number) {
-        this.json.ammount = ammount
-    }
-    lightID(id: number) {
-        this.json.id = id
-    }
-    lightType(type: number) {
-        this.json.type = type
-    }
-    sceneName(name: ModelScene) {
-        const scene = new ModelScene(new Geometry())
-        if(name === undefined) {
-            // deno-lint-ignore no-unused-vars
-            const name = scene
-        }
-        this.json.name = name
-    }
-    matName(mat: string) {
-        this.json.matname = mat
-    }
-    constructor(object: Geometry | Environment, scale?: [number, number, number], anchor?: [number, number, number]) {
-        const lasers = this.json.ammount;
-        const laserTracks: string[] = [];
-        const laserEnv = object
-        if(scale === undefined) {
-            const scale = [1, 1, 1]
-            this.json.scale = scale
-        }
-        if(anchor === undefined) {
-            const anchor = [0, 0, 0]
-            this.json.anchor = anchor
-        }
-        this.json.scale = scale
-        this.json.anchor = anchor
-
-        for (let i = 1; i <= lasers; i++) laserTracks.push(`${this.json.matname}`, `${i}`);
-
-        this.json.sceneName.addPrimaryGroups(
-            laserTracks,
-            laserEnv,
-            this.json.scale,
-            this.json.anchor
-        );
-        if(logFunctionss) {
-            RMLog(`Added new lightGroup...\nobject: ${object}\nammount: ${this.json.ammount}`)
-        }
-    }
+type groupSettings = {
+    addGroup: boolean,
+    sceneName: ModelScene,
+    transForm?: Vec3
 }
-/**
- * add an object to a primaryGroup efficiently
- * @param sceneName the name of your model scene
- * @param matName the name of your material in blender
- * @param object the object to add to your scene primarygroups
- */
 
-export class objectGroup {
-
-    json: Json = {}
-
-    import(json: Json) {
-        this.json = json
-        return this
+export class laserGroup {
+    constructor(
+        public object: GroupObjectTypes,
+        public amount: number,
+        public blenderMatName: string,
+        public lightID: number,
+        public lightType: number,
+        public addGroup?: groupSettings
+    ) {
+        this.object = object
+        this.amount = amount
+        this.blenderMatName
+        this.lightID = lightID
+        this.lightType = lightType
+        this.addGroup = addGroup
     }
+    push() {
+        const lasers = this.amount;
+        const laserTracks: string[] = [];
+        const laserEnv = this.object
+        laserEnv.lightID = this.lightID
+        laserEnv.lightType = this.lightType
+        for (let i = 1; i <= lasers; i++) laserTracks.push(`${this.blenderMatName}${i}`);
 
-    /**your model scene name */
-    sceneName(name: ModelScene) {
-        this.json.name = name
-    }
-    /** the name of your material in blender */
-    matName(name: string) {
-        this.json.matNamee = name
-    }
-    /**
-     * @param object your primary group object to add   
-     * @param scale the scale offset of your object
-     */
-    constructor(object: GroupObjectTypes, scale?: [number, number, number]) {
-        this.json.name.addPrimaryGroups(
-            this.json.matNamee,
-            object,
-            scale
-        )
+        if(this.addGroup) {
+            this.addGroup.sceneName.addPrimaryGroups(
+                this.blenderMatName,
+                this.object,
+                this.addGroup.transForm
+            )
+        }
+        if(logFunctionss) { console.log(`new light group with object ${this.object}`)}
     }
 }
