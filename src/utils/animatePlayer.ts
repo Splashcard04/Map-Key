687ee58a98e763d3440c14d1710ebddcd15792e2
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2022-12-30 14:13:35 +1030

    Update animatePlayer.ts
    Massive efficiency overhaul

--- src/utils/animatePlayer.ts
+++ src/utils/animatePlayer.ts
@@ -1,63 +1,42 @@
-import { KeyframesVec3, CustomEvent, RMLog, Json } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
+import { KeyframesVec3, CustomEvent, RMLog } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
 import { allBetween, logFunctionss } from './general.ts' 
 
 
 export class playerAnim {
-  
-     /**
-   * Class to help with animating the player and the notes together.
-   * @param time The time to start the animation.
-   * @param duration The duration of the animation.
-   * @param position Any position keyframes to add.
-   * @param rotation Any rotation keyframes to add.
-   * @param playerTrack (optional) The track to assign the player to. This must not be the same as noteTrack. Or if noteTrack is undefined, playerTrack must not be "playerAnimNote".
-   * @param noteTrack (optional) The track to assign the notes. This can be a pre-existing track that you have already assigned the notes.
-   * @author splashcard__ & Aurellis
-   */
+    /**
+     * A class to help with animating the player and the notes together.
+     * @param time The time to start the animation (defaults to 0 if left empty).
+     * @param timeEnd The time to end the animation.
+     * @param position The position keyframes for the player animation.
+     * @param rotation The rotation keyframes for the player animation.
+     * @param playerTrack The track to assign the player (defaults to "player" if left empty).
+     * @param noteTrack The track to assign to the notes (defaults to "notes" if left empty).
+     * @author splashcard__ & Aurellis
+     */
+   constructor(public time: number = 0, public timeEnd: number = 0, public position: KeyframesVec3 | undefined = undefined, public rotation: KeyframesVec3 | undefined = undefined, public playerTrack: string = "player", public noteTrack: string = "notes") {}
+   push() {
+       const duration = this.timeEnd - this.time
+       new CustomEvent(this.time).assignPlayerToTrack(this.playerTrack).push();
 
-    json: Json = {}
+       const track = new CustomEvent(this.time).animateTrack(this.playerTrack, duration)
+       if(this.position){
+        track.animate.position = this.position
+       }
+       if(this.rotation){
+        track.animate.rotation = this.rotation
+       }
+       track.push();
 
-    import(json: Json) {
-        this.json = json
-        return this
-    }
+       new CustomEvent(this.time).assignTrackParent([this.noteTrack], this.playerTrack).push();
 
-    constructor(time: number, timeEnd: number) {
-        this.json.time = time
-        this.json.timeEnd = timeEnd
-    }
-
-    position(position: KeyframesVec3) { this.json.position = position }
-    rotation(rotation: KeyframesVec3) { this.json.rotation = rotation }
-
-    playerTrack(track: string) { 
-        if(track === undefined) track = "player"
-
-        this.json.playerTrack = track
-    }
-    noteTrack(track: string) {
-        if(track === undefined) track = "notes"
-
-        this.json.playerTrack = track
-    }
-
-    push() {
-        const duration = this.json.timeEnd - this.json.time
-        new CustomEvent(this.json.time).assignPlayerToTrack(`${this.json.playerTrack}`).push();
-
-        const track = new CustomEvent(this.json.time).animateTrack(`${this.json.playerTrack}`, duration)
-        track.animate.position = this.json.position;
-        track.animate.rotation = this.json.rotation;
-        track.push();
-
-        new CustomEvent(this.json.time).assignTrackParent([`${this.json.noteTrack}`], `${this.json.playerTrack}`)
-
-        allBetween(this.json.time, this.json.timeEnd, x => {
-            x.customData.track = `${this.json.noteTrack}`
-        });
-        if(logFunctionss){
-            RMLog(`Added new player animation at beat ${this.json.time} until beat ${this.json.timeEnd}...`)
+       allBetween(this.time, this.timeEnd, x => {
+        if(!x.track.has(this.noteTrack)){
+            x.track.add(this.noteTrack)
         }
-    }
+       });
+       if(logFunctionss){
+        RMLog(`Added new player animation at beat ${this.time} until beat ${this.timeEnd}...`)
+       }
+   }
 
 }
