740e30c3c9ac9fb909542f7586c306d4fa59a61b
iamsplash <111317032+Splashcard04@users.noreply.github.com>
2022-12-29 23:17:08 -0800

    convert to class

--- src/builders/despawnerBuillder.ts
+++ src/builders/despawnerBuillder.ts
@@ -9,15 +9,18 @@ export type despawnerSettings = {
     restore?: string[]
 }
 
-export function despawnerBuilder(settings: despawnerSettings) {
-    /**
-     * @param {LOOKUP} lookup the lookup method of despawned and restored environment pieces
-     * @param {string[]} ids the array of environment ids to despawn
-     * @param { string[] } hardDespawn environment pieces to set to active = false
-     * @param {string[]} restore environment peices to restore from being despawned
-     * @author splashcard
-     */
-    const x = new despawner(settings.lookup, settings.ids, settings.hardDespawn, settings.restore).push();
+
+export class despawnerBuilder {
+    constructor(settings: despawnerSettings) {
+        /**
+         * @param {LOOKUP} lookup the lookup method of despawned and restored environment pieces
+         * @param {string[]} ids the array of environment ids to despawn
+         * @param { string[] } hardDespawn environment pieces to set to active = false
+         * @param {string[]} restore environment peices to restore from being despawned
+         * @author splashcard
+         */
+    new despawner(settings.lookup, settings.ids, settings.hardDespawn, settings.restore).push();
 
     if(logFunctionss) { console.log(`new despawner built using ${settings.lookup}`)}
+    }
 }
