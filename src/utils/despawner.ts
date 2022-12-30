4087e373925985fabc5d93e03daab1a545d25d7b
iamsplash <111317032+Splashcard04@users.noreply.github.com>
2022-12-29 22:38:13 -0800

    tweak and simplify despawner class

--- src/utils/despawner.ts
+++ src/utils/despawner.ts
@@ -1,122 +1,38 @@
-import { Environment, LOOKUP, RMLog } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
 import { logFunctionss } from "./general.ts"
+import { Environment, LOOKUP, Json } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
 
 export class despawner {
-    public lookup?: LOOKUP = "Contains";
-    /**
-     * A class to aid in the despawning of environment pieces.
-     * @param lookup The lookup method to use.
-     * @param ids The ids of the pieces to despawn.
-     * @author splashcard__
-     */
-    constructor(lookup?: LOOKUP, public ids?: string[]) {
-        this.lookup = lookup;
-        this.ids = ids;
-        if(logFunctionss){
-            RMLog("Despawner added...")
-        }
-    }
-    /**
-     * After running despawn(), this can restore Environment objects back to [0,0,0] for later use.
-     * It cannot restore objects that were despawned using hardDespawn()
-     * @param ids The ids of the objects to restore.
-     */
-    restore(ids: string[]) {
-        // Setting objects as `active = true` after running hardDespawn doesn't work sadly.
-        ids.forEach(id =>{
-            const env = new Environment(id, this.lookup);
-            env.position = [0,0,0]
-            env.push()
-        });
-        if(logFunctionss){
-            RMLog(`Despawner restored ${ids.length} environments...`)
-        }
-    }
-    /**
-     * Despawns objects using the `active` property.
-     * Useful for objects that don't respond to the `position` property.
-     */
-    hardDespawn(){
-        this.ids?.forEach(id =>{
-            const env = new Environment(id, this.lookup);
-            env.active = false;
-            env.push()
-        });
-        if(logFunctionss){
-            RMLog(`Despawner hard-despawned ${this.ids?.length} environments...`)
-        }
-    }
-    /**
-     * Despawns objects using the `position` property.
-     */
-    despawn(){
-        this.ids?.forEach(id =>{
-            const env = new Environment(id, this.lookup);
-            env.position = [-69420,-69420,-69420];
-            env.push()
-        });
-        if(logFunctionss){
-            RMLog(`Despawner removed ${this.ids?.length} environments...`)
-        }
-    }
-}
+    json: Json = {}
 
-export class advDespawner {
-    public ids: [string, LOOKUP][] = [];
-    /**
-     * A class to aid in the despawning of objects. Handles each object separately with per-object lookup.
-     * @param ids The objects to despawn, and the lookup to use for each object.
-     * @example new advDespawner([["Environment","Contains"],["PlayersPlace$","Regex"]]).despawn();
-     * @author splashcard__
-     */
-    constructor(ids?: [string, LOOKUP][]) {
-        if(ids){
-            this.ids = ids
-        }
-        if(logFunctionss){
-            RMLog("Advanced Despawner added...")
-        }
+    import(json: Json) {
+        this.json = json
+        return this
     }
-    /**
-     * Despawns objects using the `active` property.
-     * Useful for objects that don't respond to the `position` property.
-     */
-    hardDespawn() {
+
+    constructor(public lookup: LOOKUP, public ids: string[], public restore?: string[], public hardDespawn?: string[]) {
+        this.lookup = lookup
+        this.ids = ids
+        this.restore = restore
+        this.hardDespawn = hardDespawn
+    }
+
+    push() {
         this.ids.forEach(id => {
-            const env = new Environment(id[0], id[1]);
-            env.active = false;
-            env.push()
-        });
-        if(logFunctionss){
-            RMLog(`Despawner hard-despawned ${this.ids?.length} environments...`)
-        }
-    }
-    /**
-     * After running despawn(), this can restore Environment objects back to [0,0,0] for later use.
-     * It cannot restore objects that were despawned using hardDespawn()
-     * @param ids The ids of the objects to restore.
-     */
-    restore(ids: [string, LOOKUP][]) {
-        ids.forEach(ex => {
-            const env = new Environment(ex[0], ex[1]);
-            env.position = [0, 0, 0];
-            env.push()
-        });
-        if(logFunctionss){
-            RMLog(`Despawner restored ${ids.length} environments...`)
-        }
-    }
-    /**
-     * Despawns objects using the `position` property.
-     */
-    despawn(){
-        this.ids.forEach(id =>{
-            const env = new Environment(id[0], id[1]);
-            env.position = [-69420,-69420,-69420];
-            env.push()
-        });
-        if(logFunctionss){
-            RMLog(`Despawner removed ${this.ids?.length} environments...`)
-        }
+            const env = new Environment(id, this.lookup)
+            env.position = [-9999, -9999, -9999]
+            env.push();
+        })
+        this.hardDespawn?.forEach(hd => {
+            const env = new Environment(hd, this.lookup)
+            env.active = false
+            env.push();
+        })
+        this.restore?.forEach(res => {
+            const env = new Environment(res, this.lookup)
+            env.position = [0, 0, 0]
+            env.active = true
+            env.push();
+        })
+        if(logFunctionss) { console.log(`new despawner using ${this.lookup}`)
     }
 }
