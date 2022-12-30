8b7f476234dc13061dfae91dc09ba55111203455
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2022-12-31 00:52:18 +1030

    Revert "Update despawner.ts"
    This reverts commit 2d63a51b89587d94b298dcfa3845ed4070b567c3.

--- src/utils/despawner.ts
+++ src/utils/despawner.ts
@@ -1,23 +1,20 @@
 import { logFunctionss } from "./general.ts"
-import { Environment, LOOKUP, Json } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
+import { Environment, LOOKUP } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
 
 export class despawner {
-    json: Json = {}
 
-    import(json: Json) {
-        this.json = json
-        return this
+    constructor(public lookup: LOOKUP, public ids: string[], public restore?: string[], public hardDespawn?: string[]) {
+        this.lookup = lookup
+        this.ids = ids
+        this.restore = restore
+        this.hardDespawn = hardDespawn
     }
-
-    constructor(public ids: [LOOKUP,string[]][] = [], public restore: [LOOKUP,string[]][] | undefined = undefined, public hardDespawn: [LOOKUP,string[]][] | undefined = undefined) {} // Empty constructor wooooo!
 
     push() {
         this.ids.forEach(id => {
-            id.forEach(subid =>{
-                const env = new Environment("", id[0])
-                env.position = [-9999, -9999, -9999]
-                env.push();
-            })
+            const env = new Environment(id, this.lookup)
+            env.position = [-9999, -9999, -9999]
+            env.push();
         })
         this.hardDespawn?.forEach(hd => {
             const env = new Environment(hd, this.lookup)
@@ -32,4 +29,10 @@ export class despawner {
         })
         if(logFunctionss) { console.log(`new despawner using ${this.lookup}`)}
     }
+}
+
+// A slightly different advDespawner, that doesnt have to have a LOOKUP written from every id
+
+export class advDespawner {
+
 }
