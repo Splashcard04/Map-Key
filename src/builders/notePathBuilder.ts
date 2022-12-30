ecf6a38dca58a0e2059358255029041b4eb7da99
iamsplash <111317032+Splashcard04@users.noreply.github.com>
2022-12-29 23:18:50 -0800

    note filter builder class

---
+++ src/builders/notePathBuilder.ts
@@ -0,0 +1,26 @@
+import { notesBetween, Note, Json } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
+import { noteSplit } from '../utils/notePath.ts'
+import { logFunctionss } from '../utils/general.ts'
+
+export type notePathSettings = {
+    time: number,
+    timeEnd: number,
+    left: (n: Note) => void,
+    right: (n: Note) => void
+}
+
+export class notePathBuilder{
+    constructor(settings: notePathSettings) {
+    /**
+     * @param {number} time the time to begin filtering notes
+     * @param {number} timeEnd the time to stop filtering notes
+     * @param left variable pass for the left notes
+     * @param right variable pass for the right notes
+     * @author splashcard
+     */
+
+    new noteSplit(settings.time, settings.timeEnd, settings.left, settings.right)
+
+    if(logFunctionss) { console.log(`new note split path at ${settings.time} until ${settings.timeEnd}`)}
+    }
+}
