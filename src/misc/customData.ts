92bc70d9ecf1b79413e96dabcaa771b87ae6376b
iamsplash <111317032+Splashcard04@users.noreply.github.com>
2022-12-30 22:10:26 -0800

    oopsy doopsy

--- src/misc/customData.ts
+++
@@ -1,7 +0,0 @@
-import { Wall, Note } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
-
-export class customData{
-   constructor(forData: (d: Wall | Note | Environment){
-      forData(d)
-   }
-}
