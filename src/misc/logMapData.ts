991f1554375920f95a1676543ce62ce04ab41ea4
iamsplash <111317032+Splashcard04@users.noreply.github.com>
2022-12-30 22:34:11 -0800

    added map name, modded map data and colors

--- src/misc/logMapData.ts
+++ src/misc/logMapData.ts
@@ -1,18 +1,23 @@
 import { activeDiff } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
 
-export function logMapData(objects?: boolean, environment?: boolean, fakeArray?: boolean) {
-    const map = activeDiff
-    
-    if(objects !== undefined && objects === true) {
-        console.log('\n======================Objects==========================\n')
-        console.log(`\n Notes:`, map.notes.length, `\n Walls:`, map.walls.length, `\n bombs:`, map.bombs.length)
-    }
-    if(environment !== undefined && environment === true) {
-        console.log(`==========================Environment======================`)
-        console.log(`\n Environment:`, map.customData.environment)
-    }
-    if(fakeArray !== undefined && fakeArray === true) {
-        console.log(`========================fake objects=======================`)
-        console.log(`\n Fake notes:`, map.fakeNotes.length, `\n Fake walls:`, map.fakeWalls.length, `\n Fake bombs:`, map.fakeBombs.length, `\n Fake Chains: `, map.fakeChains.length)
+export class functionLogger {
+    constructor(objects: boolean = true, fakeArray: boolean = true, moddedMapData: boolean = true) {
+        const map = activeDiff
+
+        console.log(`
+        \x1b[3m. ================ ${map.name} ================
+        `)
+
+        if(objects) { console.log(
+        `\x1b[36m ======== Object Data ========`, '\n', `\x1b[32m notes: ${map.notes.length} \n walls: ${map.walls.length} \n bombs: ${map.bombs.length} \n arcs: ${map.arcs.length} \n chains: ${map.chains.length}`
+        )}
+
+        if(fakeArray) { console.log(
+            `\x1b[36m ======== Fake Array ========`, '\n', `\x1b[32m fake notes: ${map.fakeNotes.length} \n fake walls: ${map.fakeWalls.length} \n fake bombs: ${map.fakeBombs.length} \n fake chains: ${map.fakeChains}`
+        )}
+
+        if(moddedMapData) { console.log(
+            `\x1b[36m ======== Modded Map Data ========`, '\n', `\x1b[32m animateTracks: ${map.animateTracks.length} \n path animations: ${map.assignPathAnimations.length} \n parentTracks: ${map.assignTrackParents.length} \n player tracks: ${map.assignPlayerToTracks.length}`
+        )}
     }
 }
