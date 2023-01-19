57259a6b356f59a4d81cb5f6c7aa2119b981d31f
splashcard <111317032+Splashcard04@users.noreply.github.com>
2023-01-19 21:14:52 +0000

    added warning in js despawner file

--- src/JSMapper/despawner.js
+++ src/JSMapper/despawner.js
@@ -12,7 +12,7 @@ class despawner {
      */
     constructor(settings = { lookup: "Contains", ids: [], hardDespawn: [], restore: [] }) {
         if(!settings.lookup) { this.lookup = "Regex" } else { this.lookup = settings.lookup }
-        if(!settings.ids) { this.ids = ["bruh"] } else { this.ids = settings.ids }
+        if(!settings.ids) { this.ids = ["bruh"]; MKLog("Parameter `Ids` was undefined - `IDs` had been defaulted to 'buh'", false, true) } else { this.ids = settings.ids }
 
         if(!settings.hardDespawn) { this.hardDespawn = ["idfk"]} else { this.hardDespawn = settings.hardDespawn }
         if(!settings.restore) { this.restore = ['bruh']} else { this.restore = settings.restore }
