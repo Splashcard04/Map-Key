1dbfa5bd8b0aebe04a89f7611792b286bf913f2e
splashcard <111317032+Splashcard04@users.noreply.github.com>
2023-01-21 08:41:55 -0800

    new multi env

--- src/ReMapper/utils/multiEnv.ts
+++ src/ReMapper/utils/multiEnv.ts
@@ -1,32 +1,42 @@
-import { Environment, LOOKUP } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
-import { logFunctionss, MKLog } from "./general.ts";
-
+import { Json, Environment } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
+import { MKlog, logFunctionss } from '../constants.ts'
+ 
 export class multiEnv {
-    /**
-     * A class to aid in the manipulation of multiple environment pieces at once.
-     * @param ids The ids for your environment pieces. Including the lookup that should be used for each.
-     * @param forEach The code to execute on each env piece. Don't push your envs in here, They will be pushed when you use the .push() method on this class.
-     * @example new multiEnv([["Sun","Contains"],["Clouds$","Regex"]],(env) => { env.active = false; }).push();
-     * @author Aurellis
-     */
-    constructor(public ids: [string, LOOKUP][], public forEach?: (x: Environment) => void){
-        this.ids = ids;
-        this.forEach = forEach;
+    json: Json = {}
+
+    import(json: Json) {
+        this.json = json
+        return this
     }
-    /**
-     * Pushes the envs to the diff.
-     * If you have a `push()` statement in the `forEach` section, your environments will be pushed twice.
-     */
-    push(){
-        this.ids.forEach(x =>{
-            const env = new Environment(x[0],x[1]);
-            if(this.forEach){
-                this.forEach(env);
-            }
-            env.push();
-        });
-        if(logFunctionss){
-            MKLog(`Added a multi-environment with ${this.ids.length} ids...`)
-        }
+
+    get contains() { return this.json.contains }
+    set contains(ids: string[]) { this.json.contains = ids }
+
+    get regex() { return this.json.regex } 
+    set regex(ids: string[]) { this.json.regex = ids }
+
+    get exact() { return this.json.exact }
+    set exact(ids: string[]) { this.json.exact = ids }
+
+    push(forEnv: (x: Environment) => void) {
+        this.json.contains.forEach((id: string) => {
+            const env = new Environment(id, "Contains")
+            forEnv(env)
+            env.push()
+        })
+
+        this.json.regex.forEach((id: string) => {
+            const env = new Environment(id, "Regex")
+            forEnv(env)
+            env.push()
+        })
+
+        this.json.exact.forEach((id: string) => {
+            const env = new Environment(id, "Regex")
+            forEnv(env)
+            env.push()
+        })
+
+        if(logFunctionss) { MKlog("new multi env created")}
     }
 }
