88c8a5fc80d87504c8ee065ecdc1a994003d1c42
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2023-01-22 21:59:58 +1030

    Cleanup

--- src/ReMapper/utils/noteFilter.ts
+++ src/ReMapper/utils/noteFilter.ts
@@ -1,39 +1,6 @@
 import { Note, activeDiffGet } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
 import { localRotation, NOTE_FILTER_PROPS, rotation } from "../constants.ts"
 import { logFunctionss, MKLog } from "./general.ts" 
-// /**
-// * filters notes at certain positions
-// * @param timeStart the time your track will start
-// * @param timeEnd the duration of your track
-// * @param positions the position to select
-// * @author splashcard
-// */
-
-// // export class noteFilter {
-
-// //     json: Json = {}
-
-// //     import(json: Json) {
-// //         this.json = json
-// //         return this
-// //     }
-
-// //   constructor(timeStart: number, timeEnd: number, positions: Vec2[], forNote: (n: Note) => void) {
-// //     this.json.positions = positions
-// //     notesBetween(timeStart, timeEnd, n => {
-// //       let pass = false;
-// //       this.json.positions.forEach((p: [number, number]) => {
-// //         if (n.x === p[0] && n.y === p[1]) pass = true;
-// //       });
-// //       if (pass) forNote(n);
-// //     });
-// //     if(logFunctionss) {
-// //       MKLog(`Added new Note Filter at ${timeStart} until ${timeEnd}...\nUsing positions: ${this.json.positions}`)
-// //     }
-// //   }
-// // }
-
-// Reworking this to be similar to the env and geo filters.
 
 /**
  * Works like notesBetween. Except it searches for notes based on a specific note property.
