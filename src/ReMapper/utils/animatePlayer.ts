2eee09eb3cc6f5c6c639b8459c3016f2deb29b10
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2023-01-22 22:06:39 +1030

    Remove Json dependancy

--- src/ReMapper/utils/animatePlayer.ts
+++ src/ReMapper/utils/animatePlayer.ts
@@ -1,13 +1,8 @@
-import { CustomEvent, CustomEventInternals, Json } from "https://deno.land/x/remapper@3.1.1/src/mod.ts";
+import { CustomEvent, CustomEventInternals } from "https://deno.land/x/remapper@3.1.1/src/mod.ts";
 import { allBetween, logFunctionss, MKLog } from "./general.ts";
 
 export class playerAnim {
-    json: Json = {}
 
-    import(json: Json) {
-        this.json = json
-        return this
-    }
     /**
      * a class to animate notes and the player at once
      * @param time the time to start animating the player
@@ -15,39 +10,39 @@ export class playerAnim {
      * @param forTrack assign data to the track to assign player / notes to
      * @author @Splashcard @Aurellis
      */
-    constructor(time: number, timeEnd: number, forTrack: (x: CustomEventInternals.AnimateTrack) => void) {
-        this.json.time = time
-        this.json.timeEnd = timeEnd
-        this.json.forTrack = forTrack
+    constructor(public time: number = 0, public timeEnd: number = 0, public forTrack?: (x: CustomEventInternals.AnimateTrack) => void) {
     }
 
-    get playerTrack() { return this.json.playerTrack }
-    set playerTrack(track: string) { this.json.playerTrack = track }
+    get playerTrack() { return this.playerTrack }
+    set playerTrack(track: string) { this.playerTrack = track }
 
-    get noteTrack() { return this.json.noteTrack }
-    set noteTrack(track: string) { this.json.noteTrack = track }
+    get noteTrack() { return this.noteTrack }
+    set noteTrack(track: string) { this.noteTrack = track }
     
 
     push() {
 
-        const anim = new CustomEvent(this.json.time).animateTrack("player", this.json.timeEnd - this.json.time);
-        this.json.forTrack(anim);
-        anim.push();
+        if(!this.playerTrack) {
+            this.playerTrack = "player"
+        }
 
-        if(!this.json.playerTrack) {
-            this.json.playerTrack = "player"
+        if(this.forTrack){
+            const anim = new CustomEvent(this.time).animateTrack(this.playerTrack, this.timeEnd - this.time);
+            this.forTrack(anim);
+            anim.push();
         }
-        if(!this.json.noteTrack) { 
-            this.json.noteTrack = "notes"
+
+        if(!this.noteTrack) { 
+            this.noteTrack = "notes"
         }
-        new CustomEvent(this.json.time).assignPlayerToTrack(this.json.playerTrack).push()
-        new CustomEvent(this.json.time).assignTrackParent(this.json.noteTrack, this.json.playerTrack).push()
-        allBetween(this.json.time, this.json.timeEnd, n => {
+        new CustomEvent(this.time).assignPlayerToTrack(this.playerTrack).push()
+        new CustomEvent(this.time).assignTrackParent([this.noteTrack], this.playerTrack).push()
+        allBetween(this.time, this.timeEnd, n => {
             n.track.add(this.noteTrack)
         })
 
         if(logFunctionss){
-            MKLog(`Added new player animation at beat ${this.json.time} until beat ${this.json.timeEnd}...`)
+            MKLog(`Added new player animation at beat ${this.time} until beat ${this.timeEnd}...`)
         }
     }
 }
