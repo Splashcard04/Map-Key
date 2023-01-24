0c2c2e0a7db2623950b7bd652038b569aaa3c99c
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2023-01-24 13:00:22 +1030

    Fix Events

--- src/ReMapper/utils/Events.ts
+++ src/ReMapper/utils/Events.ts
@@ -1,74 +1,53 @@
-import { activeDiffGet, Json, Eade, ColorType } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
+import { ColorType, EASE, Event } from "https://deno.land/x/remapper@3.1.1/src/mod.ts";
 
 export class gradient {
-    json: Json = {}
-
-    import(json: Json) {
-        this.json = json
-        return this
-    }
-    constructor(time: number, duration: number, type: number, colors: [[1, 1, 1, 1], [1, 1, 1, 1]], easing?: ) {
-        this.json.time = time
-        this.duration = duration
-        this.json.type = type
-
-        this.json.startColor = colors[0]
-        this.json.endColor colors[1]
-    }
-
+    constructor(public time = 0, public duration = 1, public type = 0, public colors: ColorType[], public lerpType?: "HSV" | "RGB", public easing?: EASE){}
     push() {
-        activeDiffGet().basicBeatmapEvents().push({
-            "b": this.json.time,
-            "t": this.json.type,
-            "customData": {
-                "lightGradient": {
-                    "duration": this.json.duration,
-                    "startColor": this.json.startColor,
-                    "endColor": this.json.endColor
+        const startE = new Event(this.time).backLasers().on(this.colors[0])
+        startE.type = this.type;
+        startE.push()
+        let i = 0;
+        this.colors.forEach(color =>{
+            if(i !== 0){
+                const inE = new Event(i*this.duration/(this.colors.length-1)).backLasers().in(color)
+                inE.type = this.type;
+                if(this.easing){
+                    inE.easing = this.easing
                 }
+                if(this.lerpType){
+                    inE.lerpType = this.lerpType
+                }
+                inE.push()
             }
+            i++
         })
     }
 }
 
 export class strobe {
-    json: Json = {}
+    constructor(public time: number, public duration: number) {}
 
-    import(json: Json){ this.json = json; return this }
-    constructor(time: number, duration: number) {
-        this.json.time = time
-        this.duration = duration
-    }
+    get interval() { return this.interval }
+    set interval(interval: number) { this.interval = interval }
 
-    get interval() { return this.json.interval }
-    set interval(interval: number) { this.json.interval = interval }
+    get type() { return this.type }
+    set type(type: number) { this.type = type }
 
-    get type() { return this.json.type }
-    set type(type: number) { this.json.type = type }
-
-    get color() { return this.json.color }
-    set color(color: ColorType) { this.json.color = color }
+    get color() { return this.color }
+    set color(color: ColorType) { this.color = color }
 
     push() {
-        const off = time + 1 / interval
-        for(let 1 = 0; i < this.json.interval / this.json.duration; i++) {
-            activeDiffGet().basicBeatmapEvents.push({
-                "b": this.json.time + 1 / this.json.interval,
-                "t": this.json.type,
-                "customData": {
-                    "color": this.json.color
-                }
-            })
+        const off = this.time + 1 / this.interval
+        for(let i = 0; i < this.interval / this.duration; i++) {
+            const ev = new Event(this.time + 1/this.interval).backLasers().on(this.color)
+            ev.type = this.type
+            ev.push()
         }
 
-        for(let i = 0; i < this.json.interval / this.json.duration; i++) {
-            activeDiffGet().basicBeatmapEvents.push({
-                "b": off + 1 / this.json.interval,
-                "t": this.json.type,
-                "customData": {
-                    "color": this.json.color
-                }
-            })
+        for(let i = 0; i < this.interval / this.duration; i++) {
+            const ev = new Event(off + 1/this.interval).backLasers().off();
+            ev.type = this.type;
+            ev.push()
         }
     }
 }
