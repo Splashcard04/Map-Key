7ddd0cbfe784c0bd184c9682139b568948603072
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2023-01-25 13:52:51 +1030

    Move note filter to filters.ts

--- src/ReMapper/utils/filters.ts
+++ src/ReMapper/utils/filters.ts
@@ -1,5 +1,5 @@
-import { Geometry, activeDiffGet, Environment } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
-import { GEO_FILTER_PROPS, ENV_FILTER_PROPS, position, rotation, scale } from "../constants.ts";
+import { Geometry, activeDiffGet, Environment, Note } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
+import { GEO_FILTER_PROPS, ENV_FILTER_PROPS, position, rotation, scale, NOTE_FILTER_PROPS, localRotation } from "../constants.ts";
 import { logFunctionss, MKLog } from "./general.ts";
 
 /**
@@ -64,4 +64,40 @@ export function filterGeometry(property: GEO_FILTER_PROPS | position | rotation 
     if(logFunctionss){
       MKLog(`Filtered ${activeDiffGet().geometry(arr =>{arr.length})} environments for objects with a ${property} of ${value}...\nobjects found: ${count}`)
     }
+}
+
+/**
+ * Works like notesBetween. Except it searches for notes based on a specific note property.
+ * @param property The property to check for on each note. This can either be an auto-fill property like "position", or it can be one of the available enums (like rotation.x).
+ * @param value The value that the property must be to pass.
+ * @param forEach What to execute for each note that passes.
+ * @author Aurellis
+ */
+export function filterNotes(property: NOTE_FILTER_PROPS | rotation | localRotation, value: number[] | string | number | boolean, forEach: (x: Note) => void){
+  let count = 0;
+  if(property == "fake" && value == true){
+    activeDiffGet().fakeNotes.forEach((n: Note) =>{
+      forEach(n);
+      count++
+    })
+  }
+  if(property == "track"){
+    activeDiffGet().notes.forEach((n: Note) =>{
+      if(n.track.has(value.toString())){
+        forEach(n)
+        count++
+      }
+    })
+  }
+  else {
+    activeDiffGet().notes.forEach((n: Note) =>{
+      if(eval(`n.${property}.toString()`) == value.toString()){
+        forEach(n)
+        count++
+      }
+    })
+  }
+  if(logFunctionss){
+    MKLog(`Filtered ${activeDiffGet().fakeNotes.length+activeDiffGet().notes.length} notes for any with a ${property} property of ${value}...\nobjects found: ${count}`)
+  }
 }
