3b767bf0b8c930534060430b124b7d2d75c0a2e6
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2023-01-25 13:42:12 +1030

    Create events.ts

---
+++ src/ReMapper/utils/events.ts
@@ -0,0 +1,53 @@
+import { ColorType, EASE, Event } from "https://deno.land/x/remapper@3.1.1/src/mod.ts";
+
+export class gradient {
+    constructor(public time = 0, public duration = 1, public type = 0, public colors: ColorType[], public lerpType?: "HSV" | "RGB", public easing?: EASE){}
+    push() {
+        const ev = new Event(this.time).backLasers().on(this.colors[0])
+        ev.type = this.type;
+        ev.push()
+        let i = 0;
+        this.colors.forEach(color =>{
+            if(i !== 0){
+                const ev = new Event(i*this.duration/(this.colors.length-1)).backLasers().in(color)
+                ev.type = this.type;
+                if(this.easing){
+                    ev.easing = this.easing
+                }
+                if(this.lerpType){
+                    ev.lerpType = this.lerpType
+                }
+                ev.push()
+            }
+            i++
+        })
+    }
+}
+
+export class strobe {
+    constructor(public time: number, public duration: number) {}
+
+    get interval() { return this.interval }
+    set interval(interval: number) { this.interval = interval }
+
+    get type() { return this.type }
+    set type(type: number) { this.type = type }
+
+    get color() { return this.color }
+    set color(color: ColorType) { this.color = color }
+
+    push() {
+        const off = this.time + 1 / this.interval
+        for(let i = 0; i < this.interval / this.duration; i++) {
+            const ev = new Event(this.time + 1/this.interval).backLasers().on(this.color)
+            ev.type = this.type
+            ev.push()
+        }
+
+        for(let i = 0; i < this.interval / this.duration; i++) {
+            const ev = new Event(off + 1/this.interval).backLasers().off();
+            ev.type = this.type;
+            ev.push()
+        }
+    }
+}
