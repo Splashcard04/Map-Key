ee59a9ffc12ff19d354f3528c2c99b6b359e3350
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2023-01-27 22:09:29 +1030

    Add playerAnim

--- src/HeckLib/playerAnimation.ts
+++ src/HeckLib/playerAnimation.ts
@@ -1,1 +1,51 @@
-import { filter, AnimateTrack } from './HeckLib/main.ts'
+import { AnimateTrack, AssignPlayerToTrack, AssignTrackParent } from './HeckLib/main.ts'
+import { MKLog, logFunctionss } from '../ReMapper/mod.ts';
+import { allBetween } from "./general.ts";
+
+export class playerAnim {
+
+    /**
+     * A class to animate notes and the player at once
+     * @param time the time to start animating the player
+     * @param timeEnd the time to stop animating the player
+     * @param animation assign data to the track to assign player / notes to
+     * @author @Splashcard @Aurellis
+     */
+    constructor(public time: number = 0, public timeEnd: number = 0, public animation?: (x: AnimateTrack) => void) {}
+
+    get playerTrack() { return this.playerTrack }
+    set playerTrack(track: string) { this.playerTrack = track }
+
+    get noteTrack() { return this.noteTrack }
+    set noteTrack(track: string) { this.noteTrack = track }
+    
+
+    push() {
+        if(!this.playerTrack) {
+            this.playerTrack = "player"
+        }
+
+        if(this.animation){
+            const anim = new AnimateTrack(this.time, {
+                track: this.playerTrack,
+                duration: this.timeEnd - this.time
+            });
+            this.animation(anim)
+            anim.push()
+
+
+        }
+        if(!this.noteTrack) { 
+            this.noteTrack = "notes"
+        }
+        new AssignPlayerToTrack(this.time,this.playerTrack).push()
+        new AssignTrackParent(this.time,{childrenTracks: [this.noteTrack], parentTrack: this.playerTrack})
+        allBetween(this.time, this.timeEnd, n => {
+            n.data.track = this.noteTrack
+        })
+
+        if(logFunctionss){
+            MKLog(`Added new player animation at beat ${this.time} until beat ${this.timeEnd}...`)
+        }
+    }
+}
