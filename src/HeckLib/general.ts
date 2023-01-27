74c16e2514300220f58b1fa2dcd8f7ddc8acd68c
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2023-01-27 22:00:56 +1030

    Add allBetween

--- src/HeckLib/general.ts
+++ src/HeckLib/general.ts
@@ -1,9 +1,37 @@
-import { Note, activeOutput } from "./src/main.ts";
+import { Note, activeOutput } from "./HeckLib/main.ts";
 
 export function notesBetween(time: number, timeEnd: number, forNote: (n: Note) => void) {
-    JSON.parse(Deno.readTextFileSync(activeOutput)).colorNotes.forEach(x => {
-        if(x.b >= time && x.b <= timeEnd) {
+    JSON.parse(Deno.readTextFileSync(activeOutput)).colorNotes.forEach((x: Note) => {
+        if(x.time >= time && x.time < timeEnd) {
             forNote(x)
+        }
+    })
+}
+
+export function allBetween(time: number, timeEnd: number, forAll: (n: Note) => void) {
+    JSON.parse(Deno.readTextFileSync(activeOutput)).colorNotes.forEach((x: Note) => {
+        if(x.time >= time && x.time < timeEnd) {
+            forAll(x)
+        }
+    })
+    JSON.parse(Deno.readTextFileSync(activeOutput)).bombNotes.forEach((x: Note) => {
+        if(x.time >= time && x.time < timeEnd) {
+            forAll(x)
+        }
+    })
+    JSON.parse(Deno.readTextFileSync(activeOutput)).sliders.forEach((x: Note) => {
+        if(x.time >= time && x.time < timeEnd) {
+            forAll(x)
+        }
+    })
+    JSON.parse(Deno.readTextFileSync(activeOutput)).burstSliders.forEach((x: Note) => {
+        if(x.time >= time && x.time < timeEnd) {
+            forAll(x)
+        }
+    })
+    JSON.parse(Deno.readTextFileSync(activeOutput)).customData.fakeColorNotes.forEach((x: Note) => {
+        if(x.time >= time && x.time < timeEnd) {
+            forAll(x)
         }
     })
 }
