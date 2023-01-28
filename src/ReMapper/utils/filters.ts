1924585deb871b5165940076909ecd85757940e3
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2023-01-28 15:21:30 +1030

    New filter syntax.
    Now allows multiple properties and values on each filter.

--- src/ReMapper/utils/filters.ts
+++ src/ReMapper/utils/filters.ts
@@ -3,67 +3,71 @@ import { GEO_FILTER_PROPS, ENV_FILTER_PROPS, position, rotation, scale, NOTE_FIL
 import { logFunctionss, MKLog } from "./general.ts";
 
 /**
- * Works like notesBetween. Except it searches for geometry, with a set value for any property on the object as the filter.
- * @param property The property to check for on each geometry object. This can either be an autofill property like "position", or it can be one of the available enums (like position.x).
- * @param value The value that the property must be to pass.
- * @param forEach What to execute for each object that passes.
+ * Works like notesBetween, except it searches for geometry with multiple properties and values for those properties.
+ * @param filter The [[property, value], [property, value]] etc. that the geometry must have to pass.
+ * @param forEach What to execute for each geometry piece that passes.
  * @author Aurellis
  */
-export function filterGeometry(property: GEO_FILTER_PROPS | position | rotation | scale, value: number[] | string | number, forEach: (x: Geometry) => void){
-    let count = 0
-    activeDiffGet().geometry((arr: Geometry[]) =>{
-      if(property === "track"){
-        arr.forEach(x =>{
-          if (x.track.has(value.toString())){
-              forEach(x);
-              count++
+export function filterGeometry(filter: [GEO_FILTER_PROPS | position | rotation | scale, number[] | string | number][], forEach: (x: Geometry) => void) {
+  let count = 0;
+  activeDiffGet().geometry((arr: Geometry[]) => {
+    arr.forEach(geo =>{
+      let pass = true
+      filter.forEach(fill => {
+        if(fill[0] == "track"){
+          if(!geo.track.has(fill[1].toString())){
+            pass = false;
           }
-        })
-      }
-      else {
-        arr.forEach((x) =>{
-          if(eval(`x.${property}.toString()`) == value.toString()){
-              forEach(x);
-              count++
+        }
+        else {
+          if(eval(`geo.${fill[0]}.toString()`) !== fill[1].toString()){
+            pass = false
           }
-        })
+        }
+      })
+      if(pass){
+        forEach(geo)
+        count++
       }
     })
-    if(logFunctionss){
-      MKLog(`Filtered ${activeDiffGet().geometry(arr =>{arr.length})} environments for objects with a ${property} of ${value}...\nobjects found: ${count}`)
+  })
+  if(logFunctionss){
+    MKLog(`Filtered ${activeDiffGet().geometry(arr =>{arr.length})} geometry... Objects found: ${count}...`)
   }
 }
-  
-  /**
-   * Works like notesBetween. Except it searches for environments, with a set value for any property on the object as the filter.
-   * @param property The property to check for on each environment object. This can either be an auto-fill property like "position", or it can be one of the available enums (like position.x).
-   * @param value The value that the property must be to pass.
-   * @param forEach What to execute for each object that passes.
-   * @author Aurellis
-   */
-  export function filterEnvironments(property: ENV_FILTER_PROPS | position | rotation | scale, value: number[] | string | number, forEach: (x: Environment) => void){
-    let count = 0
-    activeDiffGet().environment((arr: Environment[]) =>{
-      if(property === "track"){
-        arr.forEach(x =>{
-          if (x.track.has(value.toString())){
-              forEach(x);
-              count++
+
+/**
+ * Works like notesBetween, except it searches for environments with multiple properties and values for those properties.
+ * @param filter The [[property, value], [property, value]] etc. that the environments must have to pass.
+ * @param forEach What to execute for each environment that passes.
+ * @author Aurellis
+ */
+export function filterEnvironments(filter: [ENV_FILTER_PROPS | position | rotation | scale, number[] | string | number][], forEach: (x: Environment) => void) {
+  let count = 0;
+  activeDiffGet().environment((arr: Environment[]) => {
+    arr.forEach(env =>{
+      let pass = true
+      filter.forEach(fill => {
+        if(fill[0] == "track"){
+          if(!env.track.has(fill[1].toString())){
+            pass = false;
           }
-        })
-      }
-      else {
-        arr.forEach((x) =>{
-          if(eval(`x.${property}.toString()`) == value.toString()){
-              forEach(x);
-              count++
+        }
+        else {
+          if(eval(`env.${fill[0]}.toString()`) !== fill[1].toString()){
+            pass = false
           }
-        })
+        }
+      })
+      if(pass){
+        forEach(env)
+        count++
       }
     })
-    if(logFunctionss){
-      MKLog(`Filtered ${activeDiffGet().geometry(arr =>{arr.length})} environments for objects with a ${property} of ${value}...\nobjects found: ${count}`)
-    }
+  })
+  if(logFunctionss){
+    MKLog(`Filtered ${activeDiffGet().environment(arr =>{arr.length})} environments... Objects found: ${count}...`)
+  }
 }
 
 /**
@@ -76,7 +80,7 @@ export function filterNotes(filter: [NOTE_FILTER_PROPS | rotation | localRotatio
   let fake = false
   let count = 0
   filter.forEach(fill =>{
-    if(fill[0] == "fake" && fill[1] == true){
+    if(fill[0] == "fake" && fill[1]){
       fake = true
     }
   })
@@ -84,14 +88,16 @@ export function filterNotes(filter: [NOTE_FILTER_PROPS | rotation | localRotatio
     activeDiffGet().fakeNotes.forEach((n: Note) =>{
       let pass = true;
       filter.forEach(fill =>{
-        if(fill[0] == "track"){
-          if(!n.track.has(fill[1].toString())){
-            pass = false;
+        if(fill[0] !== "fake"){
+          if(fill[0] == "track"){
+            if(!n.track.has(fill[1].toString())){
+              pass = false;
+            }
           }
-        }
-        else{
-          if(eval(`n.${fill[0]}.toString()`) !== fill[1].toString()){
-            pass = false
+          else{
+            if(eval(`n.${fill[0]}.toString()`) !== fill[1].toString()){
+              pass = false
+            }
           }
         }
       })
