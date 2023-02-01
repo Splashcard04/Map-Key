358a8ec152c152f9ea558183c63d879ed1516769
Aurellis <62498557+Aureliona1@users.noreply.github.com>
2023-02-01 16:35:40 +1030

    Update mod.ts

--- src/ReMapper/mod.ts
+++ src/ReMapper/mod.ts
@@ -10,7 +10,7 @@ export * from './utils/sceneGroup.ts'
 export * from './utils/shapeGenerator.ts'
 export * from './utils/filters.ts'
 export * from './utils/multiEnv.ts'
-export * from './utils/exportAsUserSharedEnv.ts'
+export * from './utils/userSharedEnvs.ts'
 
 export * from './misc/logMapData.ts'
 export * from './misc/random.ts'
