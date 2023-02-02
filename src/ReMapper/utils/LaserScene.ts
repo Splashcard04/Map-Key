4b8d486dfdd338d2b41032cb508286f0bd9fda1e
splashcard <111317032+Splashcard04@users.noreply.github.com>
2023-02-02 18:38:39 +0000

    laser scene

---
+++ src/ReMapper/utils/LaserScene.ts
@@ -0,0 +1,30 @@
+import { ModelScene, GroupObjectTypes, Environment, Geometry } from "https://deno.land.x.remapper@3.1.1/src/mod.ts"
+
+export class laserScene {
+    constructor(public sceneName: ModelScene, public object: GroupObjectTypes) {
+        this.sceneName = sceneName
+        this.object = object
+    }
+
+    modify(public envMod: (x: Environment | Geometry)) {
+        this.mod = envMod
+    }
+
+    matName(public name: string) {
+        this.mat = name
+    }
+
+    push() {
+        const laserMats: [] = []
+        const obj = this.object
+        this.mod(obj)
+        for(let i = 1; i <= 1; i++) {
+            laserTracks.push(this.mat+`${i}`)
+        }
+        this.sceneName.addPrimaryGroups(laserMats, obj)
+    }
+}
+
+new laserScene(scene, x => {
+    x.lightID = 0
+})
