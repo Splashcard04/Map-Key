c9127508bfece7bb546b11d80988882558aad535
Aurellis <62498557+Aureliona1@users.noreply.github.com>
2023-02-06 20:32:27 +1030

    Add graphs to functionLogger

--- src/ReMapper/misc/logMapData.ts
+++ src/ReMapper/misc/logMapData.ts
@@ -1,4 +1,5 @@
 import { activeDiff } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
+import { graphDistribution } from "./random.ts";
 
 export class functionLogger {
     /**
@@ -7,23 +8,65 @@ export class functionLogger {
      * @param fakeArray log fake object data to the console?
      * @param moddedMapData log modded map data to the console?
      */
-    constructor(objects: boolean = true, fakeArray: boolean = true, moddedMapData: boolean = true) {
+    constructor(public objects: boolean = true, public fakeArray: boolean = true, public moddedMapData: boolean = true) {}
+    run(graph?: boolean){
         const map = activeDiff
 
         console.log(`
         \x1b[3m. ================ ${map.name} ================
         `)
 
-        if(objects) { console.log(
-        `\x1b[36m ======== Object Data ========`, '\n', `\x1b[32m notes: ${map.notes.length} \n walls: ${map.walls.length} \n bombs: ${map.bombs.length} \n arcs: ${map.arcs.length} \n chains: ${map.chains.length}`
-        )}
+        if(this.objects) {
+            console.log(`\x1b[36m ======== Object Data ========`, '\n', `\x1b[32m notes: ${map.notes.length} \n walls: ${map.walls.length} \n bombs: ${map.bombs.length} \n arcs: ${map.arcs.length} \n chains: ${map.chains.length}`);
+            if(graph){
+                console.log(`\x1b[32mMap note frequency:\n\x1b[31mStart:\x1b[97m`)
+                let arr: number[] = [];
+                map.notes.forEach(note =>{
+                    arr.push(note.time)
+                })
+                graphDistribution(arr, 20, 100);
+                console.log("\x1b[31mEnd...");
+                console.log("\n\x1b[32mMap wall frequency:\n\x1b[31mStart:\x1b[97m")
+                arr = [];
+                map.walls.forEach(wall =>{
+                    arr.push(wall.time)
+                })
+                graphDistribution(arr, 20, 100);
+                console.log("\x1b[31mEnd...");
+            }
+        }
 
-        if(fakeArray) { console.log(
-            `\x1b[36m ======== Fake Array ========`, '\n', `\x1b[32m fake notes: ${map.fakeNotes.length} \n fake walls: ${map.fakeWalls.length} \n fake bombs: ${map.fakeBombs.length} \n fake chains: ${map.fakeChains}`
-        )}
+        if(this.fakeArray) {
+            console.log(`\x1b[36m ======== Fake Array ========`, '\n', `\x1b[32m fake notes: ${map.fakeNotes.length} \n fake walls: ${map.fakeWalls.length} \n fake bombs: ${map.fakeBombs.length} \n fake chains: ${map.fakeChains}`)
+            if(graph){
+                console.log(`\x1b[32mMap note frequency:\n\x1b[31mStart:\x1b[97m`)
+                let arr: number[] = [];
+                map.fakeNotes.forEach(note =>{
+                    arr.push(note.time)
+                })
+                graphDistribution(arr, 20, 100);
+                console.log("\x1b[31mEnd...");
+                console.log("\n\x1b[32mMap wall frequency:\n\x1b[31mStart:\x1b[97m")
+                arr = [];
+                map.fakeWalls.forEach(wall =>{
+                    arr.push(wall.time)
+                })
+                graphDistribution(arr, 20, 100);
+                console.log("\x1b[31mEnd...");
+            }
+        }
 
-        if(moddedMapData) { console.log(
-            `\x1b[36m ======== Modded Map Data ========`, '\n', `\x1b[32m animateTracks: ${map.animateTracks.length} \n path animations: ${map.assignPathAnimations.length} \n parentTracks: ${map.assignTrackParents.length} \n player tracks: ${map.assignPlayerToTracks.length}`
-        )}
+        if(this.moddedMapData) {
+            console.log(`\x1b[36m ======== Modded Map Data ========`, '\n', `\x1b[32m animateTracks: ${map.animateTracks(arr =>{ arr.length })} \n path animations: ${map.assignPathAnimations(arr =>{ arr.length })} \n parentTracks: ${map.assignTrackParents(arr =>{ arr.length })} \n player tracks: ${map.assignPlayerToTracks(arr =>{ arr.length })}`)
+            if(graph){
+                const array: number[] = [];
+                map.animateTracks(arr =>{
+                    arr.forEach(anim =>{
+                        array.push(anim.time)
+                    })
+                })
+                graphDistribution(array, 20, 100);
+            }
+        }
     }
 }
