88be2017bc62fc65ef68e9ca35763ddb66d71217
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2023-02-11 01:08:42 +1030

    optimizeMaterials works now.

--- src/ReMapper/utils/geoMaterial.ts
+++ src/ReMapper/utils/geoMaterial.ts
@@ -1,4 +1,4 @@
-import { Geometry, GEO_TYPE, Json, ModelScene, activeDiffGet, ColorType, GEO_SHADER, RawGeometryMaterial } from "https://deno.land/x/remapper@3.1.1/src/mod.ts" 
+import { Geometry, GEO_TYPE, Json, ModelScene, activeDiffGet, ColorType, GEO_SHADER, RawGeometryMaterial, copy } from "https://deno.land/x/remapper@3.1.1/src/mod.ts" 
 import { shaderKeywords } from "../constants.ts"
 import { logFunctionss, MKLog } from './general.ts'
 
@@ -84,29 +84,54 @@ export class Material {
     }
 }
 
+export type namingMethods = 
+    "Numbered" |
+    "By Properties" |
+    "Geometry Type Numbered" |
+    "Shader Numbered"
+
 /**
  * Converts all identical materials on geometry into a single map-wide material.
+ * @param namingMethod Decides the way to name the created materials. Defaults to numbered.
  */
-export function optimiseMaterials(){
+export function optimizeMaterials(namingMethod: namingMethods = "Numbered"){
     activeDiffGet().geometry(arr =>{
-        let i = 0
+        let i = 0;
         arr.forEach(geo =>{
+            let copied = false
             if(typeof geo.material !== "string"){
-                const mat = geo.material as RawGeometryMaterial
-                const stringMat = `${geo.material.color?.join("")},${geo.material.shader},${geo.material.shaderKeywords?.join("")}`
+                const mat = copy(geo.material)
+                let name: string
+                if(namingMethod == "By Properties"){
+                    name = `${mat.color?.join()}${mat.shader}${mat.shaderKeywords?.join()}${mat.track}`
+                }
+                else if(namingMethod == "Geometry Type Numbered"){
+                    name = `${geo.type}${i}`
+                }
+                else if(namingMethod == "Shader Numbered"){
+                    name = `${mat.shader}${i}`
+                }
+                else {
+                    name = i.toString()
+                }
+                geo.material = name
                 activeDiffGet().geometry(ray =>{
-                    let j = 0;
                     ray.forEach(x =>{
-                        if(x.material == mat && i !== j){
-                            activeDiffGet().geoMaterials[stringMat] = geo.material as RawGeometryMaterial
-                            geo.material = stringMat
-                            x.material = stringMat
+                        const xmat = x.material as RawGeometryMaterial
+                        if(mat.shader == xmat.shader && mat.color == xmat.color && mat.shaderKeywords == xmat.shaderKeywords && mat.track == xmat.track){
+                            copied = true
+                            x.material = name
                         }
-                        j++
                     })
                 })
+                if(copied){
+                    activeDiffGet().geoMaterials[name] = mat;
+                    i++
+                }
+                else{
+                    geo.material = mat
+                }
             }
-            i++
         })
     })
 }
