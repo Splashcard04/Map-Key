3dfe743a89dca1a918cf6fbba7244298473ed3f8
Aurellis <62498557+Aureliona1@users.noreply.github.com>
2023-02-14 12:10:08 +1030

    Split text into 2 small classes.
    also converted fontModel type to optional array of textObjects, or relative font path.

--- src/ReMapper/utils/text.ts
+++ src/ReMapper/utils/text.ts
@@ -1,49 +1,78 @@
-import { ModelScene, Text, Geometry, RawGeometryMaterial, Json, GroupObjectTypes } from 'https://deno.land/x/remapper@3.1.1/src/mod.ts'
-/*
-/**
- * @param  {string} text Text Input
- * @param  {string} fontModel Model used for text
- * @param  {RawGeometryMaterial} material the geometry material of the created text
- * @param  {(data:Text)=>void} data Additional data for text
- * @author UG-Ecko
- 
-export function text(text: string, fontModel: string, material: RawGeometryMaterial, data?: (data: Text) => void) {
-    const txt = new Text(fontModel)
-    if (data) {
-        data(txt)
-    }
-    new ModelScene(new Geometry("Cube", material)).static(txt.toObjects(text));
-}*/
+import { ModelScene, Text, Geometry, GeometryMaterial, Wall, ColorType, Vec3 } from 'https://deno.land/x/remapper@3.1.1/src/mod.ts'
 
-
-export class textBuilder {
-    json: Json = {}
-
-    constructor(public fontModel: string, public forText: (x: Text) => void) { 
-        this.fontModel = fontModel 
-        this.forText = forText
-    }
-
-    object(obj: GroupObjectTypes) {
-        this.json.object = obj
-    }
-    switch(keyframes: [any, number][]) {
-        if(!this.json.object || this.json.object === undefined) { this.json.object = new Geometry("Cube", {
-            shader: "Standard", shaderKeywords: [], color: [1,1,1,1]
-        })}
-        const text = new Text(this.fontModel)
-
-        this.forText(text)
-
-        const scene = new ModelScene(this.json.object)
-
-        keyframes.forEach(x => {
-            x.forEach(y => {
-                this.json.anim = text.toObjects(y[0], y[1])
-            })
-        })
-
-        scene.animate(this.json.anim)
-    }
-
+type TextObject = {
+    pos: Vec3,
+    rot: Vec3,
+    scale: Vec3,
+    color?: ColorType,
+    track?: string
 }
+
+export class textToGeo {
+    /**
+     * Create quick text with geometry.
+     * @param text The text to write.
+     * @param fontModel The relative path of your font model. Or an array of text objects.
+     * @param material The material for your geometry objects.
+     * @param extraData Any extra data for the text.
+     * @author UG-Ecko
+     */
+    constructor(public text: string, public fontModel: string | TextObject[], public material: GeometryMaterial = {shader:"Standard"}, public extraData?: (data: Text) => void){}
+    push(){
+        const txt = new Text(this.fontModel)
+        if (this.extraData) {
+            this.extraData(txt)
+        }
+        new ModelScene(new Geometry("Cube", this.material)).static(txt.toObjects(this.text));
+    }
+}
+
+export class textToWall {
+    /**
+     * Create quick text with walls.
+     * @param text The text to write.
+     * @param fontModel The relative path of your font model. Or an array of text objects.
+     * @param time The time to spawn the text.
+     * @param duration The duration of the text.
+     * @param textData Any extra data for the text.
+     * @param wallData Any extra data for the walls.
+     */
+    constructor(public text: string, public fontModel: string, public time: number, public duration: number, public textData?: (data: Text) => void, public wallData?: (data: Wall) => void){}
+    push(){
+        const txt = new Text(this.fontModel)
+        if(this.textData){
+            this.textData(txt)
+        }
+        txt.toWalls(this.text, this.time, this.time+this.duration, this.wallData)
+    }
+}
+
+// export class textBuilder {
+//     json: Json = {}
+
+//     constructor(public fontModel: string, public forText: (x: Text) => void) {}
+
+//     object(obj: GroupObjectTypes) {
+//         this.json.object = obj
+//     }
+//     // deno-lint-ignore no-explicit-any
+//     switch(keyframes: [any, number][]) {
+//         if(!this.json.object) { this.json.object = new Geometry("Cube", {
+//             shader: "Standard", shaderKeywords: [], color: [1,1,1,1]
+//         })}
+//         const text = new Text(this.fontModel)
+
+//         this.forText(text)
+
+//         const scene = new ModelScene(this.json.object)
+
+//         keyframes.forEach(x => {
+//             x.forEach(y => {
+//                 this.json.anim = text.toObjects(y[0])
+//             })
+//         })
+
+//         scene.animate(this.json.anim)
+//     }
+
+// }
