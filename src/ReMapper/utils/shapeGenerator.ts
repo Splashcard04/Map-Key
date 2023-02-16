5b70af012066cba0a915f12d6797701b2f950144
Aurellis <62498557+Aureliona1@users.noreply.github.com>
2023-02-16 12:25:56 +1030

    Ring Cone :)

--- src/ReMapper/utils/shapeGenerator.ts
+++ src/ReMapper/utils/shapeGenerator.ts
@@ -33,9 +33,8 @@ export class shapeGenerator {
 
         /**
          * Push the shape to the active diff.
-         * @param returnProp If defined, the shape will not be pushed. Instead, the method will return the property of a cube in the shape. [index, property] where index is the cube, and property is either position, scale or rotation.
          */
-        push(returnProp?: [number, GEO_FILTER_PROPS]){
+        push(){
             const cube = new Geometry("Cube", this.material);
             for(let side = 0; side < this.sides; side++){
                 // Track assignment
@@ -77,21 +76,80 @@ export class shapeGenerator {
                 }
 
                 cube.rotation = [this.rotation[0],this.rotation[1],this.rotation[2]-180*angle/Math.PI];
-                if(!returnProp){
-                    cube.push();
-                }
-                else if(side == returnProp[0]){
-                    return eval(`cube.${returnProp[1]}`)
-                }
+                
+                cube.push()
                 
                 if(logFunctionss) {
                     MKLog(`New shape generated...\nsides: ${this.sides}\nradius: ${this.radius}\ntrack: ${this.track}`)
                 }
             }
         }
+        /**
+         * Returns the array of geometry instead of pushing to the diff.
+         * @param returnProp If defined, the method will return the property of a cube in the shape. [index, property] where index is the cube, and property is either position, scale or rotation.
+         * @returns Geometry array.
+         */
+        return(returnProp?: [number, GEO_FILTER_PROPS]){
+            const returnArray = []
+            const cube = new Geometry("Cube", this.material);
+            for(let side = 0; side < this.sides; side++){
+                // Track assignment
+                if(this.track && this.iterateTrack){
+                    cube.track.value = `${this.track}_${side+this.iterateOffset}`;
+                }
+                else if(this.track && !this.iterateTrack){
+                    cube.track.value = this.track;
+                }
+
+                // Determine that angle of the side (could have used rotatePoint here, but this was a bit easier)
+                const angle = Math.PI*2*side/this.sides;
+
+                // Apply the offset position to the rotated position
+                cube.position = arrAdd(rotatePoint([-Math.sin(angle)*this.radius,-Math.cos(angle)*this.radius,0],this.rotation), this.position);
+
+                /*
+                So that I remember how this math works - Aurellis
+
+                The scale is determined by using the "Length of a chord in a circle formula".
+                l = 2rsin(theta/2)
+                => rsin(theta/2) = l/2 (since l/2 is the opp of a triangle formed by r, l/2, and the line between [0,0,0] and the midpoint of l)
+                => since r = hyp and l/2 = opp, therefore sin(theta/2) = 0.5l/r.
+                This works for a chord where the ends are r distance from [0,0,0].
+
+                but our case uses a chord where the midpoint is r distance from [0,0,0],
+                therefore tan is applicable as opp/adj rather than opp/hyp since r is now adj rather than hyp.
+                giving the formula 2rTan(theta/2). where theta = 2pi/2sides or simplified pi/sides.
+
+                then, to account for the fact that the line is 2d rather than 1d,
+                the r value can either be radius-scale for the innermost corner to touch,
+                or radius+scale for the outermost corners to touch.
+                */
+                if(this.innercorners){
+                    cube.scale = [(this.radius-this.scale[1]/2)*Math.tan(Math.PI/this.sides)*2, this.scale[1], this.scale[2]];
+                }
+                else{
+                    cube.scale = [(this.radius+this.scale[1]/2)*Math.tan(Math.PI/this.sides)*2, this.scale[1], this.scale[2]];
+                }
+
+                cube.rotation = [this.rotation[0],this.rotation[1],this.rotation[2]-180*angle/Math.PI];
+                
+                returnArray.push(cube)
+                if(returnProp && side == returnProp[0]){
+                    return eval(`cube.${returnProp[1]}`)
+                }
+
+                if(logFunctionss) {
+                    MKLog(`New shape generated...\nsides: ${this.sides}\nradius: ${this.radius}\ntrack: ${this.track}`)
+                }
+            }
+            if(!returnProp){
+                return returnArray as Geometry[]
+            }
+        }
 }
 
 export class primitiveGenerator {
+    collection: Geometry[] = []
     /**
      * Generates one of a selection of primitive 3d shapes with Geometry cubes as the edges. (Currently only supports prisms)
      * @param material The geo-material to use.
@@ -117,6 +175,15 @@ export class primitiveGenerator {
     ){}
     
     /**
+     * Push the primitive shape to the active diff.
+     */
+    push(){
+        this.collection.forEach(geo =>{
+            geo.push()
+        })
+    }
+
+    /**
      * Generates the prism of any regular 2d shape.
      * @param sides The number of sides. Default - 3.
      * @param radius The radius of the 2d shape. Default - 10.
@@ -126,10 +193,16 @@ export class primitiveGenerator {
      */
     prism(sides = 3, radius = 10, length = 10, innercorners?: boolean, alignedSides?: boolean){
         const shape = new shapeGenerator(this.material,sides,radius,arrAdd(rotatePoint([0,0,-length/2],this.rotation),this.position),this.scale,this.rotation,innercorners,this.track,this.iterateTrack,this.iterateOffset)
-        shape.push();
+        let tempArr: Geometry[] = shape.return()
+        tempArr.forEach(geo =>{
+            this.collection.push(geo)
+        })
         shape.position = arrAdd(rotatePoint([0,0,length/2],this.rotation),this.position)
         shape.iterateOffset = sides+this.iterateOffset;
-        shape.push();
+        tempArr = shape.return()
+        tempArr.forEach(geo =>{
+            this.collection.push(geo)
+        })
         
         const cube = new Geometry("Cube", this.material);
         repeat(sides, side =>{
@@ -143,10 +216,10 @@ export class primitiveGenerator {
             let angle = Math.PI*2*(side+0.5)/sides
             let pos
             if(innercorners){
-                pos = rotatePoint([-Math.sin(angle)*Math.hypot(radius,(shape.push([0,"scale[0]"])+shape.push([0,"scale[1]"]))/2),-Math.cos(angle)*Math.hypot(radius,(shape.push([0,"scale[0]"])+shape.push([0,"scale[1]"]))/2),0],this.rotation)
+                pos = rotatePoint([-Math.sin(angle)*Math.hypot(radius,(shape.return([0,"scale[0]"])+shape.return([0,"scale[1]"]))/2),-Math.cos(angle)*Math.hypot(radius,(shape.return([0,"scale[0]"])+shape.return([0,"scale[1]"]))/2),0],this.rotation)
             }
             else{
-                pos = rotatePoint([-Math.sin(angle)*Math.hypot(radius,(shape.push([0,"scale[0]"])-shape.push([0,"scale[1]"]))/2),-Math.cos(angle)*Math.hypot(radius,(shape.push([0,"scale[0]"])-shape.push([0,"scale[1]"]))/2),0],this.rotation)
+                pos = rotatePoint([-Math.sin(angle)*Math.hypot(radius,(shape.return([0,"scale[0]"])-shape.return([0,"scale[1]"]))/2),-Math.cos(angle)*Math.hypot(radius,(shape.return([0,"scale[0]"])-shape.return([0,"scale[1]"]))/2),0],this.rotation)
             }
             cube.position = arrAdd(pos, this.position);
             if(alignedSides){
@@ -154,8 +227,9 @@ export class primitiveGenerator {
             }
             cube.rotation = [this.rotation[0],this.rotation[1],this.rotation[2]-180*angle/Math.PI];
             cube.scale = [this.scale[1],this.scale[1],length+this.scale[2]];
-            cube.push()
+            this.collection.push(cube)
         })
+        return this
     }
     /**
      * Generates a sphere made of rings.
@@ -164,13 +238,37 @@ export class primitiveGenerator {
      * @param segments The number of segments to make each ring out of. Default - 8.
      * @param innerCorners Makes the segments join at the inner corner of the cubes rather than the outer one.
      */
-    ringSphere(radius = 10, rings = 8, segments = 8, innerCorners?: boolean){
+    ringSphere(rings = 8, segments = 8, radius = 10, innerCorners?: boolean){
         const Ring = new shapeGenerator(this.material,segments,radius,this.position,this.scale,this.rotation,innerCorners,this.track,this.iterateTrack,this.iterateOffset)
         repeat(rings, ring =>{
             const interpos = Math.cos((ring+0.5)*Math.PI/rings)*radius
             Ring.position = arrAdd(rotatePoint([0,0,interpos],this.rotation),this.position);
             Ring.radius = easeInCirc(Math.abs(interpos/radius),radius,-radius,1)
-            Ring.push()
+            const tempArr: Geometry[] = Ring.return()
+            tempArr.forEach(geo =>{
+                this.collection.push(geo)
+            })
         })
+        return this
+    }
+    /**
+     * Generates a cone out of rings.
+     * @param rings The number of rings to make the cone from.
+     * @param segments The number of segments per ring.
+     * @param baseRadius The radius of the base of the cone.
+     * @param depth The depth of the cone.
+     * @param innerCorners Makes the segments join at the inner corner of the cubes rather than the outer one.
+     */
+    ringCone(rings = 8, segments = 4, baseRadius = 10, depth = 10, innerCorners?: boolean){
+        const Ring = new shapeGenerator(this.material,segments,baseRadius,this.position,this.scale,this.rotation,innerCorners,this.track,this.iterateTrack,this.iterateOffset)
+        repeat(rings, ring =>{
+            Ring.position = arrAdd(rotatePoint([0,0,((ring+0.5)*depth-rings/2)/rings],this.rotation),this.position)
+            Ring.radius = baseRadius-(ring*baseRadius/rings)
+            const tempArr: Geometry[] = Ring.return()
+            tempArr.forEach(geo =>{
+                this.collection.push(geo)
+            })
+        })
+        return this
     }
 }
