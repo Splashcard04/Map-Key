40599c0f843e525409da1320bd0ff0ce0226c041
splashcard <111317032+Splashcard04@users.noreply.github.com>
2023-03-01 17:44:13 +0000

    text builder

--- src/ReMapper/utils/text.ts
+++ src/ReMapper/utils/text.ts
@@ -1,31 +1,4 @@
-import { ModelScene, Text, Geometry, GeometryMaterial, Wall, ColorType, Vec3 } from 'https://deno.land/x/remapper@3.1.1/src/mod.ts'
-
-type TextObject = {
-    pos: Vec3,
-    rot: Vec3,
-    scale: Vec3,
-    color?: ColorType,
-    track?: string
-}
-
-export class textToGeo {
-    /**
-     * Create quick text with geometry.
-     * @param text The text to write.
-     * @param fontModel The relative path of your font model. Or an array of text objects.
-     * @param material The material for your geometry objects.
-     * @param extraData Any extra data for the text.
-     * @author UG-Ecko
-     */
-    constructor(public text: string, public fontModel: string | TextObject[], public material: GeometryMaterial = {shader:"Standard"}, public extraData?: (data: Text) => void){}
-    push(){
-        const txt = new Text(this.fontModel)
-        if (this.extraData) {
-            this.extraData(txt)
-        }
-        new ModelScene(new Geometry("Cube", this.material)).static(txt.toObjects(this.text));
-    }
-}
+import { ModelScene, Text, Geometry, GeometryMaterial, Wall, ColorType, Vec3, GroupObjectTypes, Json } from 'https://deno.land/x/remapper@3.1.1/src/mod.ts'
 
 export class textToWall {
     /**
@@ -47,34 +20,37 @@ export class textToWall {
     }
 }
 
-// Keeping this commented heere because it might be more effective if we can get it to work. - Aurellis
 
-// export class textBuilder {
-//     json: Json = {}
+export class textBuilder {
+    json: Json = {}
+    /**
+     * build a text to object scene
+     * @param model the name of your font model
+     * @param forText modfifcatons to the text object
+     * @method object the object to make text of
+     * @method switch the keyframes of your text
+     */
+    constructor(public model: string, public forText: (x: Text) => void) {
+        this.model = model; this.forText = forText
+    }
 
-//     constructor(public fontModel: string, public forText: (x: Text) => void) {}
+    object(obj: GroupObjectTypes) {
+        this.json.object = obj
+    }
 
-//     object(obj: GroupObjectTypes) {
-//         this.json.object = obj
-//     }
-//     // deno-lint-ignore no-explicit-any
-//     switch(keyframes: [any, number][]) {
-//         if(!this.json.object) { this.json.object = new Geometry("Cube", {
-//             shader: "Standard", shaderKeywords: [], color: [1,1,1,1]
-//         })}
-//         const text = new Text(this.fontModel)
+    switch(keyframes: [string, number][]) {
+        const text = new Text(this.model)
+        this.forText(text)
 
-//         this.forText(text)
+        const scene = new ModelScene(this.object)
 
-//         const scene = new ModelScene(this.json.object)
+        let keyframe;
 
-//         keyframes.forEach(x => {
-//             x.forEach(y => {
-//                 this.json.anim = text.toObjects(y[0])
-//             })
-//         })
+        keyframes.forEach(x => {
+            keyframe.push(text.toObjects(x[0]), x[1])
+        })
 
-//         scene.animate(this.json.anim)
-//     }
+        scene.animate(keyframe)
 
-// }
+    }
+}
