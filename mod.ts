a24faee11e98ba83973536226e01acc9cfaaaab2
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2023-08-25 17:10:34 +0930

    Now it's a wee bit nicer

--- mod.ts
+++ mod.ts
@@ -1,16 +1,17 @@
-export * from "./src/data/constants.ts";
-export * from "./src/chroma/geoMaterial.ts";
-export * from "./src/noodle/animatePlayer.ts";
 export * from "./src/chroma/despawner.ts";
 export * from "./src/chroma/events.ts";
-export * from "./src/functions/general.ts";
-export * from "./src/noodle/notePath.ts";
+export * from "./src/chroma/geoMaterial.ts";
 export * from "./src/chroma/laserScene.ts";
 export * from "./src/chroma/shapeGenerator.ts";
-export * from "./src/functions/filters.ts";
-export * from "./src/chroma/multiEnv.ts";
 export * from "./src/chroma/userSharedEnvs.ts";
-export * from "./src/functions/optimizers.ts";
+
+export * from "./src/data/constants.ts";
+
+export * from "./src/functions/filters.ts";
+export * from "./src/functions/general.ts";
 export * from "./src/functions/logMapData.ts";
+export * from "./src/functions/optimizers.ts";
 export * from "./src/functions/random.ts";
+
+export * from "./src/noodle/animatePlayer.ts";
 export * from "./src/noodle/notePresets.ts";
